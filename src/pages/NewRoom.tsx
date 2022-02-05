b7d368a56d14949fe9bd304eb1b11ac08ae58acc
Lucas <lucas190717@gmail.com>
2022-02-05 17:52:01 -0300

    #01 class03

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,18 +1,43 @@
 import React, { useContext, useEffect } from "react";
+
+//libs
+import {Link, useNavigate} from 'react-router-dom' 
+import { database } from "../services/firebase";
+//assets
 import illustrationImg from "../@types/assets/illustration.svg";
 import logoImg from '../@types/assets/logo.svg'
-import googleIconImg from '../@types/assets/google-icon.svg'
-import {Link} from 'react-router-dom' 
+//styles
 import '../styles/auth.scss'
+//components
 import { Button } from "../components/Button";
-import {AuthContext} from '../App'
+//context
+import { AuthContext } from "../contexts/AuthContext"; 
 
 export function NewRoom() {
+  const [newRoom, setNewRom] = React.useState('')
+  const navigate = useNavigate();
 
   const {user} = useContext(AuthContext)
+
+  async function handleCreateRoom(e: React.FormEvent){
+    e.preventDefault()
+    if(newRoom.trim() === ''){
+      
+      return window.alert('texto vazio')
+    }
+
+    const roomRef = database.ref('rooms')
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id
+    })
+
+    navigate(`/rooms/${firebaseRoom.key}`)
+  }
   return (
     <div id="page-auth">
-      <h1>{user?.id}</h1>
+      {/* <h1>{user?.id}</h1> */}
       <aside>
         <img src={illustrationImg} alt="ilutraçãoperguntas respostas" />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
@@ -22,10 +47,12 @@ export function NewRoom() {
           <div className="main-content">
               <img src={logoImg} alt="Letmeask" />
              <h2>Criar uma nova sala</h2>
-              <form>
+              <form onSubmit={handleCreateRoom}>
                   <input
                   type="text"
                   placeholder="Nome da sala"
+                  onChange={e => setNewRom(e.target.value)}
+                  value={newRoom}
                   />
                   <Button type="submit" >
                       Criar sala
