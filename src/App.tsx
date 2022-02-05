631e5bf234af2aa0793663ba8425ef2a47dd9515
Lucas <lucas190717@gmail.com>
2022-02-05 17:52:01 -0300

    #03 class03

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,24 @@
 import React, { createContext } from "react";
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
+import { Room } from "./pages/Room";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { auth, firebase } from "./services/firebase";
-
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined,
-  singInWithGoogle: () => Promise<void>
-}
-
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 
 function App() {
-  const [user, setUser] = React.useState<User>();
-
-  async function singInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    const result = await auth.signInWithPopup(provider)
-
-     if(result.user){
-       const {displayName, photoURL, uid} = result.user;
-
-       if(!displayName || !photoURL ){
-          throw new Error('Missing information from Google Account')
-       }
-
-       setUser({
-         id: uid,
-         name: displayName,
-         avatar: photoURL
-       })
-     }
-
-  }
 
   return (
     <BrowserRouter>
-      <AuthContext.Provider value={{ user, singInWithGoogle }}>
+    <AuthContextProvider>
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/rooms/new" element={<NewRoom />} />
+          <Route path="/rooms/:id" element={<Room />} />
         </Routes>
-      </AuthContext.Provider>
+    </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 //assets
 import illustrationImg from "../@types/assets/illustration.svg";
 import logoImg from "../@types/assets/logo.svg";
@@ -5,7 +7,7 @@ import googleIconImg from "../@types/assets/google-icon.svg";
 
 //libs
 import { useNavigate } from "react-router-dom";
-import { auth, firebase } from "../services/firebase";
+import { auth, database, firebase } from "../services/firebase";
 
 //styles
 import "../styles/auth.scss";
@@ -13,19 +15,36 @@ import "../styles/auth.scss";
 //components
 import { Button } from "../components/Button";
 import { useContext } from "react";
-import { AuthContext } from "../App";
+import { AuthContext } from "../contexts/AuthContext";
 
 export function Home() {
   const navigate = useNavigate();
   const { user, singInWithGoogle } = useContext(AuthContext);
+  const [roomCode, setRoomCode] = React.useState("");
 
   async function handleCreateRoom() {
-    
     if (!user) {
       await singInWithGoogle();
     }
 
     navigate("/rooms/new");
+  }
+
+  async function handleJoinRoom(e: React.FormEvent) {
+    e.preventDefault();
+
+    if(roomCode.trim() == ''){
+      return
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if(!roomRef.exists()){
+      alert(`Room doesn't exits`)
+      return
+    }
+
+    navigate(`/rooms/${roomCode}`)
   }
 
   return (
@@ -43,8 +62,15 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
+          <form onSubmit={handleJoinRoom}>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+              onChange={(e) => {
+                setRoomCode(e.target.value);
+              }}
+              value={roomCode}
+            />
             <Button type="submit">Entrar na sala</Button>
           </form>
         </div>
